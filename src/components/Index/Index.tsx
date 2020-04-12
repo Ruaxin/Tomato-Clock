8a18fe802f17f2c66302d57c198ae1b74eafa5f6
ruaxin <qy.xin@foxmail.com>
2020-04-12 16:28:21 +0800

    完成index的header

--- src/components/Index/Index.tsx
+++ src/components/Index/Index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import {Button} from 'antd';
-import '../../App.css';
+import { Menu, Dropdown} from 'antd';
+import {DownOutlined, UserOutlined, LogoutOutlined} from '@ant-design/icons';
+import './Index.scss';
 import axios from '../../config/axios';
+import history from '../../config/history';
 
 interface IRouter {
   history: any
@@ -10,6 +12,25 @@ interface IRouter {
 interface IIndexState {
   user: any
 }
+
+const logout = () => {
+  localStorage.setItem('x-token', '');
+  history.push('/login');
+};
+
+const menu = (
+  <Menu>
+    <Menu.Item key="1">
+      <UserOutlined/>
+      个人设置
+    </Menu.Item>
+    <Menu.Item key="2" onClick={logout}>
+      <LogoutOutlined/>
+      <UserOutlined/>
+      注销
+    </Menu.Item>
+  </Menu>
+);
 
 class Index extends React.Component<IRouter, IIndexState> {
   constructor(props: any) {
@@ -27,16 +48,16 @@ class Index extends React.Component<IRouter, IIndexState> {
     const response = await axios.get('me');
     this.setState({user: response.data});
   };
-  logout = () => {
-    localStorage.setItem('x-token', '');
-    this.props.history.push('/login');
-  };
 
   render() {
     return (
-      <div className="Component">
-        <p>欢迎，{this.state.user && this.state.user.account}</p>
-        <Button onClick={this.logout}>注销</Button>
+      <div className="Index" id="Index">
+        <header>
+          <span className="logo">LOGO</span>
+          <Dropdown overlay={menu}>
+            <span>{this.state.user && this.state.user.account}<DownOutlined style={{marginLeft:8}}/></span>
+          </Dropdown>
+        </header>
       </div>
     );
   }
