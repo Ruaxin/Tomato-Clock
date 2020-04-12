64602534fc997b1f55359ed577a1a1b6a023dc1a
ruaxin <qy.xin@foxmail.com>
2020-04-12 15:43:08 +0800

    优化代码

--- src/components/Index/Index.tsx
+++ src/components/Index/Index.tsx
@@ -8,41 +8,34 @@ interface IRouter {
 }
 
 interface IIndexState {
-  user:any
+  user: any
 }
 
-class Index extends React.Component<IRouter,IIndexState> {
+class Index extends React.Component<IRouter, IIndexState> {
   constructor(props: any) {
     super(props);
-    this.state={
-      user:{}
-    }
+    this.state = {
+      user: {}
+    };
   }
 
   async componentWillMount() {
-    await this.getMe()
+    await this.getMe();
   }
 
   getMe = async () => {
-    try {
-      const response = await axios.get('me');
-      this.setState({user:response.data})
-    } catch (e) {
-      console.error('获取用户失败');
-      // if(e.response.status === 401){
-      //   this.props.history.push('/login')
-      // }
-    }
+    const response = await axios.get('me');
+    this.setState({user: response.data});
   };
   logout = () => {
-    localStorage.setItem('x-token','')
+    localStorage.setItem('x-token', '');
     this.props.history.push('/login');
   };
 
   render() {
     return (
       <div className="Component">
-        <p>欢迎，{this.state.user&&this.state.user.account}</p>
+        <p>欢迎，{this.state.user && this.state.user.account}</p>
         <Button onClick={this.logout}>注销</Button>
       </div>
     );
