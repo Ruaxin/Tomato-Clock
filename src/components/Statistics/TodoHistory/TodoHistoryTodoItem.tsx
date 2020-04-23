ea414ac8482ca03393a48ebb0c8495aa20aeb1d0
ruaxin <qy.xin@foxmail.com>
2020-04-23 19:20:38 +0800

    完成历史任务的样式

--- src/components/Statistics/TodoHistory/TodoHistoryTodoItem.tsx
+++ src/components/Statistics/TodoHistory/TodoHistoryTodoItem.tsx
@@ -34,7 +34,7 @@ class TodoHistoryTodoItem extends React.Component<ITodoHistoryTodoItemProps> {
       time = this.props.todo.updated_at;
       action = (
         <div className="action">
-          <span onClick={() => this.updateTodo({finished: false})}>恢复</span>
+          {/*<span onClick={() => this.updateTodo({finished: false})}>恢复</span>*/}
           <span onClick={() => this.updateTodo({deleted: true})}>删除</span>
         </div>
       );
