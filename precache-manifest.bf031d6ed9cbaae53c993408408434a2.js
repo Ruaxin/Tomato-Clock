1b11876ccc389d3480c8ab3ce6d7545698e6527d
ruaxin <qy.xin@foxmail.com>
2020-04-23 20:01:07 +0800

    Updates

---
+++ precache-manifest.bf031d6ed9cbaae53c993408408434a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568d60fe8efa2f63885fc92743c0b2be",
+    "url": "/Tomato-Clock/index.html"
+  },
+  {
+    "revision": "53337549a575fef35e92",
+    "url": "/Tomato-Clock/static/css/2.7c948047.chunk.css"
+  },
+  {
+    "revision": "ba794b6e5efd89d0beba",
+    "url": "/Tomato-Clock/static/css/main.639c84e9.chunk.css"
+  },
+  {
+    "revision": "53337549a575fef35e92",
+    "url": "/Tomato-Clock/static/js/2.4503ea64.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/Tomato-Clock/static/js/2.4503ea64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba794b6e5efd89d0beba",
+    "url": "/Tomato-Clock/static/js/main.602eb796.chunk.js"
+  },
+  {
+    "revision": "6cb02c88fda63f4022d9",
+    "url": "/Tomato-Clock/static/js/runtime-main.9006e38b.js"
+  }
+]);
